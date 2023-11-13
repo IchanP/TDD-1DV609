8b047fb637184a1f72399e159ea8c813c84be4f5
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-13 14:27:37 +0100

    RED: LocationService missing country code

--- tests/LocationService.test.js
+++ tests/LocationService.test.js
@@ -1,11 +1,15 @@
+import LocationService from '../src/LocationService.ts'
+
 describe('LocationService', () => {
+/*  let locationService
   // Reinstating the LocationService class before each test
   beforeAll(() => {
     locationService = new LocationService()
-  })
+  }) */
 
   it('should throw an error if country code is not provided', () => {
     expect(() => {
+      const locationService = new LocationService()
       locationService.fetchLocationData('London').toThrow()
     })
   })
