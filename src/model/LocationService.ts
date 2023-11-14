d4fa33fe5a0513f7ea03afa3e399a26d25e5e51f
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-14 19:48:54 +0100

    REFACTOR: LocationService trim lat and long

--- src/model/LocationService.ts
+++ src/model/LocationService.ts
@@ -10,14 +10,26 @@ export class LocationService {
    *
    * @param {string} cityName - The name of the city. Example: 'London'
    * @param {string} countryCode - The country code in ISO 3166 alpha-2 format https://www.iso.org/obp/ui/#search/code/
+   * @returns {Promise<LocationData>} - Returns a promise for the latitude and longitude of the city, in 2 decimal point accuracy.
    */
   async fetchLocationData (cityName: string, countryCode: string): Promise<LocationData> {
     this.#validateCountryCode(countryCode)
     const locationProvider = new LocationProvider(cityName, countryCode)
     const latAndLong = await locationProvider.fetchLocationData()
-    latAndLong.lat = Number(latAndLong.lat.toFixed(2))
-    latAndLong.lon = Number(latAndLong.lon.toFixed(2))
-    return latAndLong
+    return this.#trimLatitudeAndLongitude(latAndLong)
+  }
+
+  /**
+   * Trims the latitude and longitude to 2 decimal places.
+   *
+   * @param {LocationData} latAndLongToTrim - The latitude and longitude to trim.
+   * @returns {LocationData} - Returns the LocationData with trimmed latitude and longitudes.
+   */
+  #trimLatitudeAndLongitude (latAndLongToTrim : LocationData) : LocationData {
+    return {
+      lat: Number(latAndLongToTrim.lat.toFixed(2)),
+      lon: Number(latAndLongToTrim.lon.toFixed(2))
+    }
   }
 
   /**
