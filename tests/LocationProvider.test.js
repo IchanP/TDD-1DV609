32d00f93bb12bc297b812a3b6fadd3a949eac83a
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-14 09:58:33 +0100

    RED: Should throw API error on non-ok response

--- tests/LocationProvider.test.js
+++ tests/LocationProvider.test.js
@@ -2,11 +2,16 @@ import { LocationProvider } from '../src/model/LocationProvider.ts'
 import { jest } from '@jest/globals'
 import 'dotenv/config'
 
+const city = 'Motala'
+const countryCode = 'SE'
+
 describe('LocationProvider', () => {
   let locationProvider
+
   beforeAll(() => {
     locationProvider = new LocationProvider()
   })
+
   it('fetches and returns latitude and longitude of first item', async () => {
     global.fetch = jest.fn(() =>
       Promise.resolve({
@@ -53,11 +58,33 @@ describe('LocationProvider', () => {
         )
       })
     )
-    const city = 'Motala'
-    const countryCode = 'SE'
     const expected = { lat: 58.5420395, lon: 15.041261 }
     const actual = await locationProvider.fetchLocationData(city, countryCode)
-    expect(fetch).toHaveBeenCalledWith(`http://api.openweathermap.org/geo/1.0/direct?q=${city},,${countryCode}&limit=5&appid=${process.env.API_KEY}`)
+    expectApiCall()
     expect(actual).toEqual(expected)
   })
+
+  it('throws an APIError when ok status is false on fetch', async () => {
+    global.fetch.mockImplementation(() =>
+      Promise.resolve({
+        ok: false,
+        /**
+         * Mocks the json method of the response object.
+         *
+         * @returns {Promise} - Returns an empty promise.
+         */
+        json: () => Promise.resolve({})
+      })
+    )
+
+    expect(async () => await locationProvider.fetchLocationData(city, countryCode)).rejects.toThrow(APIError)
+    expectApiCall()
+  })
 })
+
+/**
+ * Checks that the API was called with the correct parameters.
+ */
+function expectApiCall () {
+  expect(fetch).toHaveBeenCalledWith(`http://api.openweathermap.org/geo/1.0/direct?q=${city},,${countryCode}&limit=5&appid=${process.env.API_KEY}`)
+}
