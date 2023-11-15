faea7ab8dbb39ed485d0775affb4cb0e11e1f9a3
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-15 15:22:35 +0100

    REFACTOR: WeatherController should accept 3 args

--- tests/WeatherController.test.ts
+++ tests/WeatherController.test.ts
@@ -1,10 +1,11 @@
 import { WeatherController } from '../src/Controller/WeatherController.ts'
 
 describe('WeatherController', () => {
+  const cityInput = document.createElement('input')
+  const countryCodeInput = document.createElement('input')
+  const submitButton = document.createElement('button')
+
   it('constructor should accept three elements as argument', () => {
-    const cityInput = document.createElement('input')
-    const countryCodeInput = document.createElement('input')
-    const submitButton = document.createElement('button')
     const weatherController = new WeatherController(cityInput, countryCodeInput, submitButton)
     expect(weatherController).toBeDefined()
   })
