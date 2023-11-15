a0761b4aac8be5968c625100fbc90d4c85845190
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-15 11:32:26 +0100

    RED: WeatherFetcherFacade should call LocationService fetchLocationData

---
+++ tests/WeatherFetcherFacade.test.js
@@ -0,0 +1,28 @@
+let sut
+describe('Fetches longitude and latitude', () => {
+  let serviceLocationDataStub
+  beforeAll(() => {
+    sut = new WeatherFetcherFacade(new LocationServiceStub())
+    serviceLocationDataStub = jest.spyOn(LocationServiceStub.prototype, 'fetchLocationData')
+  })
+  it('should call LocationService', () => {
+    const city = 'Motala'
+    const countryCode = 'SE'
+    sut.fethWeatherData(city, countryCode)
+    expect(serviceLocationDataStub).toHaveBeenCalled()
+  })
+})
+
+/**
+ * Stub of LocationService.
+ */
+class LocationServiceStub {
+  /**
+   * Mocked function to fetch location data.
+   *
+   * @returns {{lat: number, lon: number}} - Returns a stubbed object.
+   */
+  fetchLocationData () {
+    return { lat: 58.54, lon: 15.04 }
+  }
+}
