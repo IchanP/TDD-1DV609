12d19fdce735325602bbe5d2f17a33051d8e6c07
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-15 16:18:08 +0100

    RFEFACTOR: should call fetchWeatherData with correct args

--- tests/WeatherController.test.ts
+++ tests/WeatherController.test.ts
@@ -4,24 +4,26 @@ import { WeatherFetcherFacade } from '../src/model/WeatherFetcherFacade.ts'
 const fetchWeatherDataMock = jest.spyOn(WeatherFetcherFacade.prototype, 'fetchWeatherData')
   .mockImplementation(() => Promise.resolve())
 
+const cityInput = document.createElement('input')
+const countryCodeInput = document.createElement('input')
+const submitButton = document.createElement('button')
+let weatherController : WeatherController
+
 describe('WeatherController', () => {
-  const cityInput = document.createElement('input')
-  const countryCodeInput = document.createElement('input')
-  const submitButton = document.createElement('button')
+  beforeAll(() => {
+    weatherController = new WeatherController(cityInput, countryCodeInput, submitButton)
+  })
 
   it('constructor should accept three elements as argument', () => {
-    const weatherController = new WeatherController(cityInput, countryCodeInput, submitButton)
     expect(weatherController).toBeDefined()
   })
   it('should have properties for cityInput, countryCodeInput and submitButton', () => {
-    const weatherController = new WeatherController(cityInput, countryCodeInput, submitButton)
     expect(weatherController.cityInput).toBeDefined()
     expect(weatherController.countryCodeInput).toBeDefined()
     expect(weatherController.submitButton).toBeDefined()
   })
 
-  it('should call fetchWeatherData on LocationService with values from cityInput and countryCodeInput', () => {
-    const weatherController = new WeatherController(cityInput, countryCodeInput, submitButton)
+  it('should call fetchWeatherData on WeatherFetcherFacade with values from cityInput and countryCodeInput', () => {
     const city = 'Motala'
     const countryCode = 'SE'
     cityInput.value = city
