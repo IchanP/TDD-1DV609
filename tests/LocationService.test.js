ee502cafc530fc484dc823162099bb48027e0993
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-15 09:32:33 +0100

    retroactively add tests for possible return values - ALREADY PASSING

--- tests/LocationService.test.js
+++ tests/LocationService.test.js
@@ -4,15 +4,13 @@ import { InvalidCountryCodeError } from '../src/model/Errors/InvalidCountryCodeE
 import { jest } from '@jest/globals'
 import { LocationProvider } from '../src/model/LocationProvider.ts'
 
-const validFetchLocationDataMock = jest.spyOn(LocationProvider.prototype, 'fetchFirstLatAndLong')
-  .mockImplementation(() => {
-    return { lat: 10.134123, lon: 42.41281380 }
-  })
+// Initial mock implementation of fetchFirstLatAndLong for 4th test case in this file.
+let validFetchLocationDataMock = mockFetchFirstLatAndLong(10.134123, 42.41281380)
 
 let sut
 const city = 'AnyCity'
 const countryCode = 'XX'
-describe('LocationService', () => {
+describe('LocationServic fetchLocationData error and calls', () => {
   beforeAll(() => {
     sut = new LocationService()
   })
@@ -33,13 +31,53 @@ describe('LocationService', () => {
     sut.fetchLocationData(city, countryCode)
     expect(validFetchLocationDataMock).toHaveBeenCalled()
   })
+})
 
-  it('fetchlocationdata should return only 2 decimals on lat and long', async () => {
-    const expected = { lat: 10.13, lon: 42.41 }
-    const actual = await sut.fetchLocationData(city, countryCode)
-    expect(actual).toEqual(expected)
+describe('LocationService fetchLocationData expected return values', () => {
+  beforeEach(() => {
+    // Clear it before each test case as the return value needs to be remocked for the different values.
+    validFetchLocationDataMock.mockClear()
+  })
+
+  it('should return 2 decimals on positive with 2 wholes ', async () => {
+    validFetchLocationDataMock = mockFetchFirstLatAndLong(10.134123, 42.41281380)
+    assertFetchLocationDataReturnsCorrectValues(10.13, 42.41)
+  })
+
+  it('should return 2 decimals on negatives with 2 wholes', async () => {
+    validFetchLocationDataMock = mockFetchFirstLatAndLong(-10.134123, -42.41281380)
+    assertFetchLocationDataReturnsCorrectValues(-10.13, -42.41)
+  })
+
+  it('should return 2 decimals on negatives with 3 wholes', async () => {
+    validFetchLocationDataMock = mockFetchFirstLatAndLong(-100.134123, -100.41281380)
+    assertFetchLocationDataReturnsCorrectValues(-100.13, -100.41)
+  })
+
+  it('should return 2 decimals on positives with 3 wholes', async () => {
+    validFetchLocationDataMock = mockFetchFirstLatAndLong(100.134123, 100.41281380)
+    assertFetchLocationDataReturnsCorrectValues(100.13, 100.41)
+  })
+
+  it('should return 2 decimals with mixed negatives and positives', async () => {
+    validFetchLocationDataMock = mockFetchFirstLatAndLong(-100.134123, 100.41281380)
+    assertFetchLocationDataReturnsCorrectValues(-100.13, 100.41)
   })
 })
+
+/**
+ * Mocks the fetch call in LocationProvider.
+ *
+ * @param {number} latitude - Should be a floating point number.
+ * @param {number} longitude - Should be a floating point number.
+ * @returns {Function} - Returns a mocked function.
+ */
+function mockFetchFirstLatAndLong (latitude, longitude) {
+  return jest.spyOn(LocationProvider.prototype, 'fetchFirstLatAndLong')
+    .mockImplementation(() => {
+      return { lat: latitude, lon: longitude }
+    })
+}
 
 /**
  * Expects fetchLocationData to throw an InvalidCountryCodeError.
@@ -48,4 +86,16 @@ describe('LocationService', () => {
  */
 async function expectAsyncFetchThrowOnInvalidCountry (countryCode) {
   expect(async () => await sut.fetchLocationData(city, countryCode)).rejects.toThrow(InvalidCountryCodeError)
+}
+
+/**
+ * Asserts that fetchLocationData returns the correct values.
+ *
+ * @param {number} latitude - The latitude.
+ * @param {number} longitude - The longitude.
+ */
+async function assertFetchLocationDataReturnsCorrectValues (latitude, longitude) {
+  const expected = { lat: latitude, lon: longitude }
+  const actual = await sut.fetchLocationData(city, countryCode)
+  expect(actual).toEqual(expected)
 }
