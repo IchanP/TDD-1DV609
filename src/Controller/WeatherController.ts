dc498bd4ac16ca28f8610011333bf4203db8100d
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-15 15:27:42 +0100

    REFACTOR: WeatherController should have properties for constructor args

--- src/Controller/WeatherController.ts
+++ src/Controller/WeatherController.ts
@@ -2,9 +2,9 @@
  * Works as the controller between the view and the model.
  */
 export class WeatherController {
-  submitButton: HTMLButtonElement
-  countryCodeInput: HTMLInputElement
-  cityInput: HTMLInputElement
+  #submitButton: HTMLButtonElement
+  #countryCodeInput: HTMLInputElement
+  #cityInput: HTMLInputElement
   /**
    * Initializes the fields of the class.
    *
@@ -13,8 +13,35 @@ export class WeatherController {
    * @param {HTMLButtonElement} submitButton - The button element for submitting the form.
    */
   constructor (cityInputElement : HTMLInputElement, countryCodeInputElement : HTMLInputElement, submitButton : HTMLButtonElement) {
-    this.cityInput = cityInputElement
-    this.countryCodeInput = countryCodeInputElement
-    this.submitButton = submitButton
+    this.#cityInput = cityInputElement
+    this.#countryCodeInput = countryCodeInputElement
+    this.#submitButton = submitButton
+  }
+
+  /**
+   * Returns the city input element.
+   *
+   * @returns {HTMLButtonElement} - Returns the private field.
+   */
+  get submitButton (): HTMLButtonElement {
+    return this.#submitButton
+  }
+
+  /**
+   * Returns the city input element.
+   *
+   * @returns {HTMLInputElement} - Returns the private field.
+   */
+  get countryCodeInput (): HTMLInputElement {
+    return this.#countryCodeInput
+  }
+
+  /**
+   * Returns the city input element.
+   *
+   * @returns {HTMLInputElement} - Returns the private field.
+   */
+  get cityInput (): HTMLInputElement {
+    return this.#cityInput
   }
 }
