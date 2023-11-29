d7bf82d6cfbc8d510697d5d08691127f1b8477bc
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-29 16:13:05 +0100

    REFACTOR: Should call using correct URL

--- tests/WeatherDataProvider.test.ts
+++ tests/WeatherDataProvider.test.ts
@@ -2,14 +2,17 @@ import { jest } from '@jest/globals'
 import { WeatherDataProvider } from '../src/model/WeatherDataProvider.ts'
 import { mockFetch } from './utils/testUtils'
 
+let sut : WeatherDataProvider
 describe('WeatherDataProvider', () => {
+  beforeAll(() => {
+    sut = new WeatherDataProvider()
+  })
   beforeEach(() => {
     jest.clearAllMocks()
   })
 
   it('it should call fetch with correct url', async () => {
     const locationData : LocationData = { lat: 58.54, lon: 15.04 }
-    const sut = new WeatherDataProvider()
     mockFetch({})
     const expected = `https://api.openweathermap.org/data/2.5/weather?lat=${locationData.lat}&lon=${locationData.lon}&appid=${process.env.API_KEY}`
     sut.fetchCurrentWeatherData(locationData)
