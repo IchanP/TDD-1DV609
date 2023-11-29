8027c6ff5cab9cdb3eb609daa3a6824c4d12f9dd
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-29 16:54:07 +0100

    RED: fetchCurrentWeatherData should call WeatherDataProvider with provided args

---
+++ src/model/__mocks__/WeatherDataProvider.ts
@@ -0,0 +1,13 @@
+/**
+ * Mock for WeatherDataProvider.
+ */
+export class WeatherDataProvider {
+  /**
+   * Mocked function to fetch weather data.
+   *
+   * @param {LocationData}  latAndLong - The location data to use to fetch.
+   */
+  fetchCurrentWeatherData (latAndLong: LocationData) {
+    // DO NOTHING FOR NOW
+  }
+}
--- tests/WeatherDataService.test.ts
+++ tests/WeatherDataService.test.ts
@@ -1,15 +1,23 @@
 import { jest } from '@jest/globals'
 import { WeatherDataService } from '../src/model/WeatherDataService'
 import { isCurrentWeather } from './utils/testUtils'
+import { WeatherDataProvider } from '../src/model/WeatherDataProvider'
+
+jest.mock('../src/model/WeatherDataProvider.ts')
 
 let sut : WeatherDataService
+const placeHolderLocationData = { lat: 0, lon: 0 }
 describe('WeatherDataService', () => {
   beforeAll(() => {
     sut = new WeatherDataService()
   })
   it('fetchCurrentWeatherData should return a CurrentWeather object', () => {
-    const placeHolderLocationData = { lat: 0, lon: 0 }
     const actual = sut.fetchCurrentWeatherData(placeHolderLocationData)
     expect(isCurrentWeather(actual)).toBe(true)
+  })
+  it('fetchCUrrentWeatherData should call WeatherDataProvider with passed argument', () => {
+    const mockedFetchCurrentWeatherData = jest.spyOn(WeatherDataProvider.prototype, 'fetchCurrentWeatherData')
+    sut.fetchCurrentWeatherData(placeHolderLocationData)
+    expect(mockedFetchCurrentWeatherData).toHaveBeenCalledWith(placeHolderLocationData)
   })
 })
