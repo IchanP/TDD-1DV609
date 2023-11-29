121397ded578a95cf965f7b4b16117b031068be3
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-30 00:21:46 +0100

    GREEN: fetchCurrentWeatherData should call with imperial if passed imperial as arg

--- src/model/WeatherDataProvider.ts
+++ src/model/WeatherDataProvider.ts
@@ -8,11 +8,12 @@ export class WeatherDataProvider implements IWeatherProvider {
    * Fetches the weather data.
    *
    * @param {LocationData} latAndLong - The necessary latitude and longitude to fetch the weather data.
+   * @param {string} unitType - The unit type to use when fetching the weather data.
    * @returns {Promise<any>} - The response from the API.
    */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  async fetchCurrentWeatherData (latAndLong: LocationData) : Promise<any> {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latAndLong.lat}&lon=${latAndLong.lon}&appid=${process.env.API_KEY}`)
+  async fetchCurrentWeatherData (latAndLong: LocationData, unitType : string = 'metric') : Promise<any> {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latAndLong.lat}&lon=${latAndLong.lon}&appid=${process.env.API_KEY}&units=${unitType}`)
     this.#validateFetch(response)
     const data = await response.json()
     return data
