f95e9bbfcb9f9166af2eaeaf68b754d5be0a7bde
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-29 16:31:02 +0100

    REFACTOR: Should throw APIError on failed fetch

--- src/model/WeatherDataProvider.ts
+++ src/model/WeatherDataProvider.ts
@@ -13,10 +13,19 @@ export class WeatherDataProvider {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async fetchCurrentWeatherData (latAndLong: LocationData) : Promise<any> {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latAndLong.lat}&lon=${latAndLong.lon}&appid=${process.env.API_KEY}`)
+    this.#validateFetch(response)
+    const data = await response.json()
+    return data
+  }
+
+  /**
+   * Validates that the repsons from the API was OK.
+   *
+   * @param {Response} response - The response object.
+   */
+  #validateFetch (response: Response) : void {
     if (!response.ok) {
       throw new APIError()
     }
-    const data = await response.json()
-    return data
   }
 }
