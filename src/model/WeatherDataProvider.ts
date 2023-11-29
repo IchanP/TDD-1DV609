8f246c741a7ecda321478e30ad089d6474db49c6
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-29 16:11:48 +0100

    GREEN: WeatherDataProvider should call using correct URL

--- src/model/WeatherDataProvider.ts
+++ src/model/WeatherDataProvider.ts
@@ -8,6 +8,6 @@ export class WeatherDataProvider {
    * @param {LocationData} latAndLong - The necessary latitude and longitude to fetch the weather data.
    */
   async fetchCurrentWeatherData (latAndLong: LocationData) {
-
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latAndLong.lat}&lon=${latAndLong.lon}&appid=${process.env.API_KEY}`)
   }
 }
