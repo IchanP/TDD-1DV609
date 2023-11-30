6c01a6525e30365276bcb9f075fa0a66e0ddf678
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-30 04:21:50 +0100

    RED: WeatherView should have currentTemperature field

--- tests/WeatherView.test.ts
+++ tests/WeatherView.test.ts
@@ -15,6 +15,9 @@ describe('WeatherView', () => {
   it('should have a field for the current weather title', () => {
     expect(sut.currentWeatherTitle).toBeDefined()
   })
+  it('should have a field for the current temperature', () => {
+    expect(sut.currentTemperature).toBeDefined()
+  })
   it('renderCurrentWeatherData should set the src attribute of the currentWeatherImage to correct value', () => {
     const expected = `https://openweathermap.org/img/wn/${mockedCurrentWeather.pictureIcon}@2x.png`
     sut.renderCurrentWeatherData(mockedCurrentWeather)
