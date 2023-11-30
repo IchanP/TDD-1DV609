730456941a2b62d6e6b7cdbff339c3fc23a1ef2a
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-30 03:43:37 +0100

    RED: WeatherView should have field for current image

--- tests/WeatherView.test.ts
+++ tests/WeatherView.test.ts
@@ -1,2 +1,10 @@
 import { jest } from '@jest/globals'
 import { mockedCurrentWeather } from './utils/testUtils'
+import { WeatherView } from '../src/View/WeatherView'
+
+const sut = new WeatherView()
+describe('WeatherView', () => {
+  it('should have a field for the current weather image', () => {
+    expect(sut.weatherImage).toBeDefined()
+  })
+})
