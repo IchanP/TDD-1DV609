5b16d0326c8003e34382e272dd0655c500b657fd
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-30 03:51:18 +0100

    RED: renderCurrentWeatherData should set src attribute to expected value

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ import { WeatherDataService } from './model/WeatherDataService'
 const cityInput = document.getElementById('cityname-input') as HTMLInputElement
 const countryCodeInput = document.getElementById('countrycode-input') as HTMLInputElement
 const submitButton = document.getElementById('gofetch-button') as HTMLButtonElement
-const weatherImage = document.getElementById('weather-image') as HTMLImageElement
+const currentWeatherImage = document.getElementById('weather-image') as HTMLImageElement
 const locationService = new LocationService()
 const dataService = new WeatherDataService()
-const view = new WeatherView()
+const view = new WeatherView(currentWeatherImage)
 const weatherController = new WeatherController(cityInput, countryCodeInput, locationService, dataService, view)
 
 submitButton.addEventListener('click', async () => {
--- tests/WeatherView.test.ts
+++ tests/WeatherView.test.ts
@@ -8,4 +8,10 @@ describe('WeatherView', () => {
   it('should have a field for the current weather image', () => {
     expect(sut.currentWeatherImage).toBeDefined()
   })
+  it('renderCurrentWeatherData should set the src attribute of the currentWeatherImage to correct value', () => {
+    const expected = `https://openweathermap.org/img/wn/${mockedCurrentWeather.pictureIcon}@2x.png`
+    sut.renderCurrentWeatherData(mockedCurrentWeather)
+    const actual = sut.currentWeatherImage.src
+    expect(actual).toBe(expected)
+  })
 })
