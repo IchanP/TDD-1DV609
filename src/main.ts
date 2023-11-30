2084a395043729277a675c4833b530e3debe88d9
Pontus Grandin <pg222pb@student.lnu.se>
2023-11-30 04:05:55 +0100

    RED: WeatherView should have field for currentWeatherTitle

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,17 @@ import { WeatherView } from './View/WeatherView'
 import { LocationService } from './model/LocationService'
 import { WeatherDataService } from './model/WeatherDataService'
 
+const currentWeatherImage = document.getElementById('weather-image') as HTMLImageElement
+
+const view = new WeatherView(currentWeatherImage)
+
 const cityInput = document.getElementById('cityname-input') as HTMLInputElement
 const countryCodeInput = document.getElementById('countrycode-input') as HTMLInputElement
-const submitButton = document.getElementById('gofetch-button') as HTMLButtonElement
-const currentWeatherImage = document.getElementById('weather-image') as HTMLImageElement
 const locationService = new LocationService()
 const dataService = new WeatherDataService()
-const view = new WeatherView(currentWeatherImage)
 const weatherController = new WeatherController(cityInput, countryCodeInput, locationService, dataService, view)
 
+const submitButton = document.getElementById('gofetch-button') as HTMLButtonElement
 submitButton.addEventListener('click', async () => {
   await weatherController.fetchWeatherData()
 })
--- tests/WeatherView.test.ts
+++ tests/WeatherView.test.ts
@@ -11,6 +11,9 @@ describe('WeatherView', () => {
   it('should have a field for the current weather image', () => {
     expect(sut.currentWeatherImage).toBeDefined()
   })
+  it('should have a field for the current weather title', () => {
+    expect(sut.currentWeatherTitle).toBeDefined()
+  })
   it('renderCurrentWeatherData should set the src attribute of the currentWeatherImage to correct value', () => {
     const expected = `https://openweathermap.org/img/wn/${mockedCurrentWeather.pictureIcon}@2x.png`
     sut.renderCurrentWeatherData(mockedCurrentWeather)
